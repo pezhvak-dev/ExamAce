a8edf8487358699d731031c53f59f3fc85c02121
Pouya <popibani1379@gmail.com>
2024-04-08 13:11:07 +0330

    Removed like count of a comment!

--- assets/js/like_comment_ajax.js
+++ assets/js/like_comment_ajax.js
@@ -1,14 +1,11 @@
 function like(id) {
     var element = document.getElementById("like");
-    var count = document.getElementById("count");
     $.get(`/weblog/comment/like/${id}`).then(response => {
-        if (response['response'] === "liked") {
-            element.className = "flex items-center justify-center relative w-9 h-9 bg-secondary rounded-full text-muted transition-colors text-red-500"
-            count.innerText = Number(count.innerText) + 1;
-        } else {
-            element.className = "flex items-center justify-center relative w-9 h-9 bg-secondary rounded-full text-muted transition-colors hover:text-red-500"
-            count.innerText = Number(count.innerText) - 1;
-
+            if (response['response'] === "liked") {
+                element.className = "flex items-center justify-center relative w-9 h-9 bg-secondary rounded-full text-muted transition-colors text-red-500"
+            } else {
+                element.className = "flex items-center justify-center relative w-9 h-9 bg-secondary rounded-full text-muted transition-colors hover:text-red-500"
+            }
         }
-    })
+    )
 }
