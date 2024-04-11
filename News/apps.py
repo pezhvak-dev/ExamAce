01634a66019ed565e4fc14adca36ff6a89e08715
Pouya <popibani1379@gmail.com>
2024-04-11 20:20:56 +0330

    Added the same signals as weblog in News app.

--- News/apps.py
+++ News/apps.py
@@ -5,3 +5,6 @@ class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'News'
     verbose_name = 'اخبار'
+
+    def ready(self):
+        import News.signals
---
+++ News/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from Account.models import NewsLetter
+from News.email import send_created_news_email_to_news_letter
+from News.models import News
+
+
+@receiver(post_save, sender=News)
+def send_news_created(sender, instance: News, created, **kwargs):
+    if created:
+        news_letter_emails = NewsLetter.objects.filter(is_blocked=False)
+        emails = list(news_letter_emails.values_list('email', flat=True))
+
+        send_created_news_email_to_news_letter(emails, f"127.0.0.1:8000/news/detail/{instance.slug}")
