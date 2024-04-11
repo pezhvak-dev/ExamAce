46e5b3886432edaee6d97ef44e1b5233817e44d6
Pouya <popibani1379@gmail.com>
2024-04-11 11:30:32 +0330

    Added exam filters to mobile and tablet view.

--- Course/filters.py
+++ Course/filters.py
@@ -8,7 +8,7 @@ class ExamFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains', label='دسته بندی')
     level = django_filters.ChoiceFilter(field_name='level', choices=Exam.level_choices_types, label='میزان سختی')
     payment_type = django_filters.ChoiceFilter(field_name='type', choices=Exam.exam_payment_types, label='نوع آزمون')
-    has_discount = django_filters.BooleanFilter(field_name='has_discount', label='تخفیف دارد؟')
+    has_discount = django_filters.BooleanFilter(field_name='has_discount', label='تخفیف')
 
     class Meta:
         model = Exam
