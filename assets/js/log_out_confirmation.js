6b9d93a0114f5c5e5a36d1c87475be7329453df1
Pouya <popibani1379@gmail.com>
2024-04-12 12:09:59 +0330

    Logout confirmation bug fixes.

--- assets/js/log_out_confirmation.js
+++ assets/js/log_out_confirmation.js
@@ -1,22 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const logoutButton = document.querySelector('.logout-button');
+    const logoutButtons = document.querySelectorAll('.logout-button');
 
-    if (logoutButton) {
-        logoutButton.addEventListener('click', function (e) {
-            e.preventDefault();
+    if (logoutButtons) {
+        logoutButtons.forEach(function (button) {
+            button.addEventListener('click', function (e) {
+                e.preventDefault();
 
-            Swal.fire({
-                title: 'خروج از حساب',
-                text: 'آیا از خروج خود اطمینان دارید؟',
-                icon: 'warning',
-                showCancelButton: true,
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'بله',
-                cancelButtonText: 'خیر'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = logoutButton.parentElement.getAttribute('href');
-                }
+                Swal.fire({
+                    title: 'خروج از حساب',
+                    text: 'آیا از خروج خود اطمینان دارید؟',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'بله',
+                    cancelButtonText: 'خیر'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        const logoutUrl = button.parentElement.getAttribute('href');
+                        if (logoutUrl) {
+                            window.location.href = logoutUrl;
+                        }
+                    }
+                });
             });
         });
     }
