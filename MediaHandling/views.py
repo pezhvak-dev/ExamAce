dab3086e3fd9820b0e55695e0541e344953c3a79
pouyabaniadam2000 <baniadampouya2000@gmail.com>
2024-05-03 02:46:12 -0700

    Added a security layer for media serving.

--- MediaHandling/views.py
+++ MediaHandling/views.py
@@ -23,15 +23,25 @@ def serve_protected_media(request, filepath):
 
         return redirect("home:home")
 
-    # try:
-    if "Course/Exam/pdf" in filepath:
-        exam = Exam.objects.get(questions_file=filepath)
-        print(exam.designer)
+    try:
+        if "Course/Exam/pdf" in filepath:
+            exam = Exam.objects.get(questions_file=filepath)
+            print(exam.designer)
 
-        has_user_participated_in_exam = user in exam.participated_users.all()
+            has_user_participated_in_exam = user in exam.participated_users.all()
 
-        if user.is_authenticated:
-            if has_user_participated_in_exam is False:
+            if user.is_authenticated:
+                if has_user_participated_in_exam is False:
+                    messages.error(request, f"شما مجوز مشاهده این فایل را ندارید!")
+
+                    redirect_url = request.session.get('current_url')
+
+                    if redirect_url is not None:
+                        return redirect(redirect_url)
+
+                    return redirect("home:home")
+
+            else:
                 messages.error(request, f"شما مجوز مشاهده این فایل را ندارید!")
 
                 redirect_url = request.session.get('current_url')
@@ -41,26 +51,16 @@ def serve_protected_media(request, filepath):
 
                 return redirect("home:home")
 
-        else:
-            messages.error(request, f"شما مجوز مشاهده این فایل را ندارید!")
+        full_filepath = os.path.join(settings.MEDIA_ROOT, filepath)
 
-            redirect_url = request.session.get('current_url')
+        return FileResponse(open(full_filepath, 'rb'))
 
-            if redirect_url is not None:
-                return redirect(redirect_url)
+    except:
+        messages.error(request, f"مشکلی از سمت سرور رخ داده است!")
 
-            return redirect("home:home")
+        redirect_url = request.session.get('current_url')
 
-    full_filepath = os.path.join(settings.MEDIA_ROOT, filepath)
+        if redirect_url is not None:
+            return redirect(redirect_url)
 
-    return FileResponse(open(full_filepath, 'rb'))
-
-    # except:
-    #     messages.error(request, f"مشکلی از سمت سرور رخ داده است!")
-
-    #     redirect_url = request.session.get('current_url')
-
-    #     if redirect_url is not None:
-    #         return redirect(redirect_url)
-
-    #     return redirect("home:home")
+        return redirect("home:home")
